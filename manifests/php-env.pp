f56214ec84a4b6bfc711faa9d02bc273f78ee18e
Vinícius Krolow <krolow@gmail.com>
2012-12-17 10:26:52 -0200

    The name has changed to php-env

--- manifests/php-env.pp
+++ manifests/php-env.pp
@@ -1,3 +1,11 @@
-include oh-my-zsh
-include php54
-include vim
+class php-env {
+    $gitUser = "krolow"
+    $gitEmail = "krolow@gmail.com"   
+
+    include php54
+    include git
+    #include vim
+    #include oh-my-zsh
+}
+
+include php-env
